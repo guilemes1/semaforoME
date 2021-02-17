7ff34af33714df69a792a61e92bccad9b1761cd5
20185189 <20185189@DSN1271158619.cfp127aluno.local>
2021-02-17 14:58:18 -0200

    Primeiro commit. Projeto semáforo Maquina de Estados.

---
+++ semaforoME.X/main.c
@@ -0,0 +1,16 @@
+/*
+ * File:   main.c
+ * Author: 20185189
+ *
+ * Created on 17 de Fevereiro de 2021, 14:55
+ */
+
+#include "config.h"
+#include <xc.h>
+
+void main(void) 
+{
+    while(1)
+    {
+    }
+}
